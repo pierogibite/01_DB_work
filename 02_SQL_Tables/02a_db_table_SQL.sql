29cde126f7739128907f905dff8c8b38aacf3b80
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-02-27 09:58:01 +0100

    Add files via upload

---
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -0,0 +1,30 @@
+/* ------  Strukturen ----- */
+
+/* Kommentar 1 */
+#  Kommentar 2
+-- Kommentar 3
+
+/* Datenbanken auf Server anzeigen */
+
+
+/* DB boo löschen, falls vorhanden*/
+
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+
+
+/* DB auswählen */
+
+/* Tabelle anlegen */
+
+
+/* Alle Tabellen in der DB anzeigen */
+
+
+/* Struktur der Tabelle anzeigen */
+
+
+/* ----- Daten ------- */
+
+
+/* ---- Inhalte der Tabelle anzeigen ---- */
