388d6955fe152bcbccda637c15369c19b91449db
Peter <schmidt.multimedia@gmail.com>
2025-02-28 12:48:00 +0100

    02a_db_table_SQL ++

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -1,3 +1,5 @@
+\! cls
+
 /* ------  Strukturen ----- */
 
 /* Kommentar 1 */
@@ -5,26 +7,35 @@
 -- Kommentar 3
 
 /* Datenbanken auf Server anzeigen */
-
+SHOW DATABASES;
 
 /* DB boo löschen, falls vorhanden*/
-
+DROP DATABASE IF EXISTS boo ;
 
 /* DB boo anlegen, falls noch nicht vorhanden*/
-
+CREATE DATABASE IF NOT EXISTS boo;
 
 /* DB auswählen */
+USE boo;
 
 /* Tabelle anlegen */
-
+CREATE TABLE IF NOT EXISTS test
+(
+    name VARCHAR(20),
+    age INT
+);
 
 /* Alle Tabellen in der DB anzeigen */
-
+SHOW TABLES;
+-- SHOW TABLES FROM mysql;
 
 /* Struktur der Tabelle anzeigen */
-
+DESCRIBE test;
 
 /* ----- Daten ------- */
-
+INSERT INTO test(name,age) VALUES("Max",35);
+INSERT INTO test(age,name) VALUES(29,"Maxine");
+INSERT INTO test VALUES();
 
 /* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM test;
