70de371c97b77cb41b6f4706e02f50f89237f050
Peter <schmidt.multimedia@gmail.com>
2025-03-03 12:07:39 +0100

    02d_db_table_SQL / Dot-Syntax

---
+++ 02_SQL_Tables/02d_db_table_SQL.sql
@@ -0,0 +1,38 @@
+\! cls
+/* ------  Strukturen ----- */
+
+/* 	 
+	Tabelle mit id als PRIMARY KEY --> 
+    KEINE Duplikate moeglich
+    AUTO_INCREMENT ++
+    DEFAULT-Werte eintragen
+*/
+
+
+/* Tabelle test löschen, falls vorhanden*/
+DROP TABLE IF EXISTS boo.test;
+
+/* Tab. test anlegen, falls noch nicht vorhanden*/
+CREATE TABLE IF NOT EXISTS boo.test
+(
+	name VARCHAR(20) NOT NULL DEFAULT "TBA",
+	age INT NOT NULL DEFAULT 0
+);
+
+/* Alle Tabellen in der DB anzeigen */
+SHOW TABLES FROM boo;
+
+/* Struktur der Tabelle anzeigen */
+DESCRIBE boo.test;
+
+/* ----- Daten ------- */
+INSERT INTO boo.test(name,age) VALUES ("Maxine",29);
+INSERT INTO boo.test(age,name) VALUES (35,"Max");
+INSERT INTO boo.test VALUES ();
+
+-- ABER: Doppelte Datensätze werden zugelassen !
+INSERT INTO boo.test(age,name) VALUES (35,"Max");
+
+/* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM boo.test;
+
